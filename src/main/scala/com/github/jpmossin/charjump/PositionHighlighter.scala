8c925a45028ba356ace0deb8e7be2d78625be9a6
Jan-Preben Mossin <jps.mossin@gmail.com>
2016-11-17 21:03:18 +0100

    remove unused import

--- src/main/scala/com/github/jpmossin/charjump/PositionHighlighter.scala
+++ src/main/scala/com/github/jpmossin/charjump/PositionHighlighter.scala
@@ -7,7 +7,6 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.editor.impl.EditorImpl
-import com.intellij.openapi.project.Project
 import com.intellij.ui.awt.RelativePoint
 
 /**
